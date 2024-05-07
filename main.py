878b11ffab5ca7c19362ef9d206fa0d37de1d01e
amoulkaf <anouar.moulkaf@gmail.com>
2024-05-07 15:57:57 +0100

    fix

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ def fetch_redirect_url(url):
     try:
         response = requests.get(url, allow_redirects=False, verify=False)
         if response.status_code in [301, 302, 303, 307, 308]:
-            return response.headers['Location']
+            return response.headers['Location'], None
+        elif response.status_code == 404:
+            logging.warning(f"Received 404 for URL {url}")
+            return None, "404"
     except Exception as e:
         logging.error(f"Error fetching redirect URL for {url}: {e}")
-    return None
+        return None, None
+    return None, None
 
 
 def fetch_app_details(app_id):
@@ -41,7 +45,9 @@ def find_version_compatibility(releases, version):
 def main():
     input_file = 'apps.csv'
     output_file = 'enhanced_app_data.csv'
+    private_apps_file = 'private_apps.csv'
     results = []
+    private_apps = []
 
     try:
         with open(input_file, newline='', mode='r') as csvfile:
@@ -52,37 +58,49 @@ def main():
                                               'is_current_version_compatible_9', 'download_link']
 
             for row in reader:
-                redirect_url = fetch_redirect_url(row['details'])
+                redirect_url, status = fetch_redirect_url(row['details'])
                 if redirect_url:
                     app_id = extract_app_id(redirect_url)
                     app_details, download_link = fetch_app_details(app_id)
-                    current_version_compatibility = find_version_compatibility(app_details.get('releases', []),
-                                                                               row['version'])
+                    if app_details:
+                        current_version_compatibility = find_version_compatibility(app_details.get('releases', []),
+                                                                                   row['version'])
 
-                    row.update({
-                        'uid': app_details.get('uid', 'N/A'),
-                        'appid': app_details.get('appid', 'N/A'),
-                        'is_archived': app_details.get('is_archived', 'N/A'),
-                        'current_version': row['version'],
-                        'latest_version': app_details.get('release', {}).get('title', 'N/A'),
-                        'release_published_time': app_details.get('release', {}).get('published_time', 'N/A'),
-                        'latest_version_compatibility': app_details.get('release', {}).get('splunk_compatibility',
-                                                                                           'N/A'),
-                        'current_version_compatibility': current_version_compatibility,
-                        'latest_version_installed': str(
-                            row['version'] == app_details.get('release', {}).get('title', 'N/A')),
-                        'is_current_version_compatible_9': str("9.1" in current_version_compatibility),
-                        'download_link': download_link
-                    })
-                    results.append(row)
+                        row.update({
+                            'uid': app_details.get('uid', 'N/A'),
+                            'appid': app_details.get('appid', 'N/A'),
+                            'is_archived': app_details.get('is_archived', 'N/A'),
+                            'current_version': row['version'],
+                            'latest_version': app_details.get('release', {}).get('title', 'N/A'),
+                            'release_published_time': app_details.get('release', {}).get('published_time', 'N/A'),
+                            'latest_version_compatibility': app_details.get('release', {}).get('splunk_compatibility',
+                                                                                               'N/A'),
+                            'current_version_compatibility': current_version_compatibility,
+                            'latest_version_installed': str(
+                                row['version'] == app_details.get('release', {}).get('title', 'N/A')),
+                            'is_current_version_compatible_9': str("9.1" in current_version_compatibility),
+                            'download_link': download_link
+                        })
+                        results.append(row)
+                elif status == "404":
+                    private_apps.append(row)
 
+        # Writing data to the main CSV
         with open(output_file, mode='w', newline='') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             writer.writerows(results)
-        logging.info(f"Data written to {output_file} successfully.")
+
+        # Writing data to the private apps CSV
+        with open(private_apps_file, mode='w', newline='') as csvfile:
+            private_fieldnames = reader.fieldnames  # Assuming original fields for private apps
+            writer = csv.DictWriter(csvfile, fieldnames=private_fieldnames)
+            writer.writeheader()
+            writer.writerows(private_apps)
+
+        logging.info(f"Data written to {output_file} and {private_apps_file} successfully.")
     except Exception as e:
-        logging.error(f"An error occurred: {e}")
+        logging
 
 
 if __name__ == "__main__":
